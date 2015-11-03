d87245b195e4221d006bc40f8f063a41edb174d7
neoligero <neoligero@gmail.com>
2015-11-03 13:45:43 +0100

    user fixes

--- models/user.js
+++ models/user.js
@@ -1,16 +1,5 @@
 var mongoose = require('mongoose');
 
-exports.signUp = function( data, callback ){
-	var user = mongoose.model("User");
-	var new_user = new user({
-		username: data.username,
-		password: data.password,
-		email: data.email
-	});
-	
-	new_user.save(function(err){
-		if(err) return next(err);
-		
-		callback();
-	});
+exports.editProfile = function( data, callback ){
+	callback();
 };
